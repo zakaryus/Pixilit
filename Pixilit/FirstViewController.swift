23e809035f3c4cd33b9c662bb0bc019f0db89144
zakaryus <Zak@Zak-Steeles-MacBook-Pro.local>
2015-01-28 20:17:25 -0800

    Initial commit
    Messing around with getting data from pixilit.com. No luck.

--- Pixilit/FirstViewController.swift
+++ Pixilit/FirstViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController,UITableViewDelegate {
+    
+    var cellText:String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,30 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Test"
+        
+        return cell
+    }
+
+    func loadData() -> Void {
+        
+        let path = "http://www.pixilit.com/rest/node/1.json"
+        
+        let url = NSURL(string: path)
+        var request = NSURLRequest(URL: url!)
+        var data = NSURLConnection.sendSynchronousRequest(request, returningResponse: nil, error: nil)
+        if data != nil {
+            var json = JSON(data: data!)
+            println(json["title"].string)
+        }
+    }
 
 }
 
