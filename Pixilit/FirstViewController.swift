65018f39289dcca8bdddaac239b993b8dab01412
zakaryus <Zak@Zak-Steeles-MacBook-Pro.local>
2015-01-28 18:35:20 -0800

    Initial Commit

---
+++ Pixilit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pixilit
+//
+//  Created by Zak Steele MBP on 1/28/15.
+//  Copyright (c) 2015 PixilitSeniorProject. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
