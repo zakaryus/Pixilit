2b583654cd028a90adbb201efe2178211eb69d5b
zakaryus <Zak@Zak-Steeles-MacBook-Pro-2.local>
2015-02-14 12:32:56 -0800

    Emptying Master to merge hopefulmaster

--- Pixilit/businessPhotosCollectionViewCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  businessPhotosCollectionViewCell.swift
-//  Pixilit
-//
-//  Created by Tyree, Spencer on 2/9/15.
-//  Copyright (c) 2015 PixilitSeniorProject. All rights reserved.
-//
-
-import Foundation
